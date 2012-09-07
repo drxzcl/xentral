9067d1e74f69e1b3536423808444a5d0c5de0d7b
drxzcl <drxzclx@gmail.com>
2012-09-08 01:20:26 +0200

    Update testsuite
    Update testsuite to test PUSH/POP

--- tools/testsuite.asm
+++ tools/testsuite.asm
@@ -43,14 +43,58 @@ MOV 0x00101010,R1
 MOV 0x00000001,R2
 MOV R1,[R2]
 MOV [R2],R3
-MOV R3-R1,R9
+MOV R3-R1,R8
 BR NZ,fail
 ; ...
  
+; Push/Pop (0x4/0x5)
+MOV 0x00101010,R1
+MOV 0x00000001,R2
+MOV SP,R4
+PUSH R1
+MOV SP,R5
+POP R3
+MOV SP,R6
+MOV R3-R1,R8
+BR NZ,fail
+; Check the stack pointer
+MOV R4-R6,R8
+BR NZ,fail
+MOV R5+R2,R5
+MOV R5-R6,R8
+BR NZ,fail
+
+; Now push a counter and pop it
+MOV R1^R1,R1
+MOV 0x00000001, R2
+MOV 0x00000020, R3
+testpushpop01:
+PUSH R1
+MOV R1+R2,R1
+MOV R3-R1, R8
+BR NZ,testpushpop01
+
+testpushpop02:
+POP R4
+MOV R1-R2,R1
+MOV R4-R1,R8
+BR NZ,fail  ; Popped value equals the counter?
+MOV R1,R1
+BR NZ,testpushpop02
+
+
+
+ 
+ 
+ 
 pass:
 MOV 0xAAAAAAAA,R1
-JMP pass
+pass01:
+JMP pass01
 
 fail:
 MOV 0xBBBBBBBB,R1
-JMP fail
+fail01:
+JMP fail01
+
+
